7d76381635bea5f0a534fe692d81f7cd6d25401c
Olivia Gilliam <75933763+oagilliam@users.noreply.github.com>
2023-05-23 19:55:22 -0400

    prompt added

--- Polygon-Area-Calculator.py
+++ Polygon-Area-Calculator.py
@@ -1,4 +1,6 @@
-
+# In this project you will use object oriented programming to create a
+# Rectangle class and a Square class. The Square class should be a subclass of
+# Rectangle and inherit methods and attributes.
 
 
 class Rectangle:
